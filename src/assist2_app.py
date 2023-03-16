f9c9768353873e9e34ef6e9b23a19a54cfa80f8d
Nilakash Das <dasnilakash@gmail.com>
2023-03-16 11:58:09 +0100

    integrate natural language prompt response functionality using openAI API

--- src/assist2_app.py
+++ src/assist2_app.py
@@ -3,9 +3,38 @@ import time
 from io import StringIO
 import sys
 import pandas as pd
+import openai
+import os
+
+# Fetch openai api key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def generate_chatgpt_response(prompt, conversation_history, system_prompt):
+    model_engine = "gpt-3.5-turbo"
+
+    # Construct the list of messages for the API
+    messages = [{"role": "system", "content": system_prompt}]
+    for entry in conversation_history:
+        user_prompt, response = entry
+        messages.append({"role": "user", "content": user_prompt})
+        if isinstance(response, str):
+            messages.append({"role": "assistant", "content": response})
+
+    # Add the current prompt
+    messages.append({"role": "user", "content": prompt})
+
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=messages,
+        max_tokens=50,
+        n=1,
+        temperature=0.7)
+
+    text_output = response["choices"][0]["message"]["content"]
+    return text_output
 
 def execute_python_statement(statement):
-    try:
+    #try:
         original_stdout = sys.stdout
         sys.stdout = captured_output = StringIO()
 
@@ -20,12 +49,18 @@ def execute_python_statement(statement):
 
         return output
 
-    except Exception as e:
-        return str(e)
+    #except Exception as e:
+    #    return str(e)
 
 def on_change():
     if st.session_state.user_input:
-        output = execute_python_statement(st.session_state.user_input)
+        try:
+            output = execute_python_statement(st.session_state.user_input)
+        except:
+            #output = generate_chatgpt_response(st.session_state.user_input)
+
+            output = generate_chatgpt_response(st.session_state.user_input, st.session_state.conversation_history, st.session_state.system_prompt)
+
         st.session_state.conversation_history.append((st.session_state.user_input, output))
         unique_key = f"user_input_{time.time()}"
         st.session_state.user_input = ""
@@ -35,17 +70,20 @@ def handle_file_upload(file):
     if file:
         df = pd.read_csv(file)
         st.session_state.locals_dict['df'] = df
-        st.session_state.conversation_history.append(("Upload CSV", "The DataFrame has been uploaded"))
+        st.session_state.conversation_history.append(("Upload CSV", "The csv has been uploaded into a pandas dataframe object called df"))
         st.experimental_rerun()
 
 def main():
-    st.title("Python Statement Executor")
+    st.title("My awesome data science assistant")
 
     if "conversation_history" not in st.session_state:
         st.session_state.conversation_history = []
 
     if "locals_dict" not in st.session_state:
         st.session_state.locals_dict = {}
+    
+    if "system_prompt" not in st.session_state:
+        st.session_state.system_prompt = "You are a data science assistant. Answer concisely and factually"
 
     if 'df' not in st.session_state.locals_dict:
         uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"], key="csv_uploader")
@@ -57,15 +95,15 @@ def main():
     st.markdown("**Conversation History:**")
     for entry in st.session_state.conversation_history:
         user_prompt, response = entry
-        st.markdown(f"- **Input**: {user_prompt}")
+        st.markdown(f"**In**: {user_prompt}")
         if isinstance(response, pd.DataFrame):
             st.dataframe(response)
         else:
-            st.markdown(f"- **Output**: {response}")
+            st.markdown(f"**Out**: {response}")
 
     user_input_placeholder = st.empty()
 
-    user_input = user_input_placeholder.text_input("Enter your Python statement:", key="user_input", on_change=on_change, args=[])
+    user_input = user_input_placeholder.text_input("Enter your Python statement or ask a question:", key="user_input", on_change=on_change, args=[])
 
 if __name__ == "__main__":
     main()
