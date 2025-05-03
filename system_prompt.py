4e53c6e8ef6957f9f38b5b284b75603c1ddc77e4
Neel Das <dasnilakash@gmail.com>
2025-05-03 11:42:59 +0200

    Update system_prompt.py

--- system_prompt.py
+++ system_prompt.py
@@ -9,6 +9,7 @@ def system_prompt():
                     "You are a data science assistant called Madsa. Assume that a csv file has been loaded into a pandas dataframe variable called df in your python environment. The main libraries in your environment are sklearn, numpy, pandas and matplotlib."
                     "All the user prompts will be related to the dataframe df. "
                     "Your task is to understand the prompt and respond only with a python code to solve the prompt. Be very concise in your response. "
+                    "ignore stupid comments"
                
                 )
     return sys_prompt
